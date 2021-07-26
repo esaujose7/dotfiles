2a9e4768fec88ef5a5ae007d5754d71756795668
Esaú Murillo <esaujose7@gmail.com>
2021-07-26 18:17:12 -0300

    wrong diagnosticls package on the lsps install

--- install-lsps.sh
+++ install-lsps.sh
@@ -1,1 +1,1 @@
-npm i -g vscode-langservers-extracted graphql-language-service-cli dockerfile-language-server-nodejs typescript typescript-language-server diagnosticls eslint_d
+npm i -g vscode-langservers-extracted graphql-language-service-cli dockerfile-language-server-nodejs typescript typescript-language-server diagnostic-languageserver eslint_d
