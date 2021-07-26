a5d38dc4c64890ce7ba183ef1206ba85c3d4b1dd
Esaú Murillo <esaujose7@gmail.com>
2021-07-25 23:20:07 -0300

    lsps that i use

---
+++ install-lsps.sh
@@ -0,0 +1,4 @@
+npm i -g vscode-langservers-extracted
+npm install -g graphql-language-service-cli
+npm install -g dockerfile-language-server-nodejs
+npm install -g typescript typescript-language-server
