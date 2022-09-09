2b487a2fa2cff534b3a4f8502da65b555d866c67
Esaú Murillo <esaujose7@gmail.com>
2022-09-09 02:43:23 -0300

    remove silent command from lsp saga

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -10,9 +10,9 @@ saga.init_lsp_saga {
   border_style = "round",
 } 
 
-nnoremap("<silent><leader>cd", ":Lspsaga show_line_diagnostics<CR>")
-nnoremap("<silent> ]e", ":Lspsaga diagnostic_jump_next<CR>")
-nnoremap("<silent> [e", ":Lspsaga diagnostic_jump_prev<CR>")
-nnoremap("<silent>K", ":Lspsaga hover_doc<CR>")
-nnoremap("<silent><leader>rr", ":Lspsaga rename<CR>")
-nnoremap("<silent><leader>cd", ":Lspsaga show_line_diagnostics<CR>")
+nnoremap("<leader>cd", ":Lspsaga show_line_diagnostics<CR>")
+nnoremap("]e", ":Lspsaga diagnostic_jump_next<CR>")
+nnoremap("[e", ":Lspsaga diagnostic_jump_prev<CR>")
+nnoremap("K", ":Lspsaga hover_doc<CR>")
+nnoremap("<leader>rr", ":Lspsaga rename<CR>")
+nnoremap("<leader>cd", ":Lspsaga show_line_diagnostics<CR>")
