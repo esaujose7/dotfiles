75fe615dbdabeae6dd07528360bb83f0cfd1ee02
Esaú Murillo <esaujose7@gmail.com>
2022-12-07 00:31:30 -0300

    remove hover.nvim

--- nvim/lua/wafle/plugins.lua
+++ nvim/lua/wafle/plugins.lua
@@ -26,26 +26,6 @@ return require('packer').startup(function(use)
     end,
     requires = { "nvim-lua/plenary.nvim" },
   }
-  use {
-    "lewis6991/hover.nvim",
-    config = function()
-        require("hover").setup {
-            init = function()
-                -- Require providers
-                require("hover.providers.lsp")
-            end,
-            preview_opts = {
-                border = nil
-            },
-            -- Whether the contents of a currently open hover window should be moved
-            -- to a :h preview-window when pressing the hover keymap.
-            preview_window = false,
-            title = true
-        }
-        -- Setup keymaps
-        vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-    end
-  }
 
   -- Auto completion
   use 'hrsh7th/nvim-cmp'
