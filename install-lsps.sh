df6a0f1cdbe0d45f5ca9bfca29439010c24ac565
Esaú Murillo <esaujose7@gmail.com>
2023-03-18 22:00:17 -0300

    improve install lsps readability

--- install-lsps.sh
+++ install-lsps.sh
@@ -1,1 +1,9 @@
-npm i -g vscode-langservers-extracted graphql-language-service-cli dockerfile-language-server-nodejs typescript typescript-language-server diagnostic-languageserver eslint_d pyright emmet-ls
+npm i -g \
+  vscode-langservers-extracted \
+  graphql-language-service-cli \
+  dockerfile-language-server-nodejs \
+  typescript typescript-language-server \
+  diagnostic-languageserver \
+  eslint_d \
+  pyright \
+  emmet-ls
