b9072ee6eb456ba55f40d44bb5e79c4b7467afac
Esaú Murillo <esaujose7@gmail.com>
2023-07-31 20:37:39 -0300

    fix

--- nvim/after/plugin/gitsigns.rc.lua
+++ nvim/after/plugin/gitsigns.rc.lua
@@ -8,34 +8,38 @@ require('gitsigns').setup {
   },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
+
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr
       vim.keymap.set(mode, l, r, opts)
     end
+
     -- Navigation
-    map(‘n’, ‘]c’, function()
-      if vim.wo.diff then return ‘]c’ end
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
-      return ‘<Ignore>’
+      return '<Ignore>'
     end, {expr=true})
-    map(‘n’, ‘[c’, function()
-      if vim.wo.diff then return ‘[c’ end
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
-      return ‘<Ignore>’
+      return '<Ignore>'
     end, {expr=true})
+
     -- Actions
-    map(‘n’, ‘ghs’, gs.stage_hunk)
-    map(‘v’, ‘ghs’, function() gs.stage_hunk {vim.fn.line(‘.’), vim.fn.line(‘v’)} end)
-    map(‘n’, ‘ghu’, gs.undo_stage_hunk)
-    map(‘n’, ‘ghr’, gs.reset_hunk)
-    map(‘v’, ‘ghr’, function() gs.reset_hunk {vim.fn.line(‘.’), vim.fn.line(‘v’)} end)
-    map(‘n’, ‘ghR’, gs.reset_buffer)
-    map(‘n’, ‘ghp’, gs.preview_hunk)
-    map(‘n’, ‘ghb’, function() gs.blame_line{full=true} end)
-    map(‘n’, ‘ghd’, gs.diffthis)
-    map(‘n’, ‘ghD’, function() gs.diffthis(‘~’) end)
+    map('n', 'ghs', gs.stage_hunk)
+    map('n', 'ghs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', 'ghu', gs.undo_stage_hunk)
+    map('n', 'ghr', gs.reset_hunk)
+    map('n', 'ghr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', 'ghR', gs.reset_buffer)
+    map('n', 'ghp', gs.preview_hunk)
+    map('n', 'ghb', function() gs.blame_line{full=true} end)
+    map('n', 'ghd', gs.diffthis)
+    map('n', 'ghD', function() gs.diffthis('~') end)
     -- Text object
-    map({‘o’, ‘x’}, ‘ih’, ‘:<C-U>Gitsigns select_hunk<CR>’)
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
