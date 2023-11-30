77bca3bc550378d95a3afcc1e25f9c525b6074a2
Esaú Murillo <esaujose7@gmail.com>
2023-11-30 19:39:20 -0300

    install lazy

--- nvim/lua/wafle/plugins.lua
+++ nvim/lua/wafle/plugins.lua
@@ -1,13 +1,8 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
+return require('lazy').setup({
   -- LSP related
-  use 'neovim/nvim-lspconfig'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use {
+  'neovim/nvim-lspconfig',
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
@@ -17,49 +12,48 @@ return require('packer').startup(function(use)
           },
       })
     end,
-    requires = { "nvim-lua/plenary.nvim" },
-  }
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
   -- Auto completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'petertriho/cmp-git'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'lukas-reineke/cmp-rg'
-  use 'onsails/lspkind-nvim'
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-nvim-lua',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'petertriho/cmp-git',
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
+  'lukas-reineke/cmp-rg',
+  'onsails/lspkind-nvim',
 
   -- telescope bro
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
+  'nvim-lua/popup.nvim',
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
 
   -- status line
-  use 'hoob3rt/lualine.nvim'
+  'hoob3rt/lualine.nvim',
 
   -- Editing related
-  use 'b3nj5m1n/kommentary'
-  use {
+  'b3nj5m1n/kommentary',
+  {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
-  }
-  use 'mhartington/formatter.nvim'
+  },
+  'mhartington/formatter.nvim',
 
   -- Git related
-  -- use 'TimUntersberger/neogit'
-  use 'tpope/vim-fugitive'
-  use 'lewis6991/gitsigns.nvim'
+   'tpope/vim-fugitive',
+   'lewis6991/gitsigns.nvim',
 
   -- File tree
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
+   'kyazdani42/nvim-web-devicons',
+   'kyazdani42/nvim-tree.lua',
 
   -- THEMES 
-  use 'navarasu/onedark.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'folke/tokyonight.nvim'
-end)
+   'navarasu/onedark.nvim',
+   { "catppuccin/nvim", name = "catppuccin" },
+   'folke/tokyonight.nvim',
+})
