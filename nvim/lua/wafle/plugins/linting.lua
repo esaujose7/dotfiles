4cf3dc4dd4bc3226887eb4693815e000f74b1834
Esaú Murillo <esaujose7@gmail.com>
2023-12-03 20:22:29 -0300

    swap null-ls for nvim-lint

--- nvim/lua/wafle/plugins/linting.lua
+++ nvim/lua/wafle/plugins/linting.lua
@@ -1,14 +1,30 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
     config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.diagnostics.eslint_d,
-        },
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        eslint = { "eslint_d" },
+        css = { "stylelint" }
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end
       })
     end,
-    dependencies = { "nvim-lua/plenary.nvim" },
   }
 }
