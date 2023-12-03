6fb993e6e669d696d3e42569f02a56e3212a59be
Esaú Murillo <esaujose7@gmail.com>
2023-12-03 20:22:43 -0300

    incorporate stylelint for formatters

--- nvim/lua/wafle/plugins/formatter.lua
+++ nvim/lua/wafle/plugins/formatter.lua
@@ -2,11 +2,32 @@ return {
   {
     'mhartington/formatter.nvim',
     config = function()
+      local util = require "formatter.util"
+
       local eslint = function()
         return {
           exe = "eslint_d",
-          args = {"--stdin", "--stdin-filename", vim.api.nvim_buf_get_name(0), "--fix-to-stdout"},
+          args = {
+            "--stdin",
+            "--stdin-filename",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--fix-to-stdout"
+          },
           stdin = true
+        }
+      end
+
+      local stylelint = function()
+        return {
+          exe = "stylelint",
+          args = {
+            "--fix",
+            "--stdin",
+            "--stdin-filename",
+            util.escape_path(util.get_current_buffer_file_path())
+          },
+          stdin = true,
+          try_node_modules = true
         }
       end
 
@@ -17,6 +38,7 @@ return {
           javascriptreact = {eslint},
           typescript = {eslint},
           typescriptreact = {eslint},
+          css = {stylelint}
         }
       })
     end,
