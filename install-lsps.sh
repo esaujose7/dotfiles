e015c550a4c11fa3a8a4631c5f41b113498d0508
Esau Murillo <esaujose7@gmail.com>
2024-07-07 18:32:07 -0300

    remove install-lsps since we use mason

--- install-lsps.sh
+++
@@ -1,9 +0,0 @@
-npm i -g \
-  vscode-langservers-extracted \
-  graphql-language-service-cli \
-  dockerfile-language-server-nodejs \
-  typescript typescript-language-server \
-  diagnostic-languageserver \
-  eslint_d \
-  pyright \
-  emmet-ls
