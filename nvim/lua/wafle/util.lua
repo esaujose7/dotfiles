204324df14a2879808aeed0ffc384761eb7b70d4
Esaú Murillo <esaujose7@gmail.com>
2025-05-20 01:29:24 -0300

    refactor on_attach to be reusable by nvim-jdtls

--- nvim/lua/wafle/util.lua
+++ nvim/lua/wafle/util.lua
@@ -368,4 +368,19 @@ function M.get_active_client_by_name(bufnr, servername)
   return vim.lsp.get_clients({ bufnr = bufnr, name = servername })[1]
 end
 
+function M.on_attach()
+  local opts = { noremap = true, silent = true }
+
+  -- LSP actions
+  vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set('n', 'K', require('hover').hover, {desc = 'hover.nvim'})
+  vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
+
+  -- Diagnostics
+  vim.keymap.set('n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+  vim.keymap.set('n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+  vim.keymap.set('n', ']e', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+end
+
 return M
