2dcffd15147ffdd985c387b17e19e8db254ab3d0
Esau Murillo <esaujose7@gmail.com>
2025-08-23 20:54:42 -0300

    configure copilotchat

--- nvim/lua/wafle/plugins/ia.lua
+++ nvim/lua/wafle/plugins/ia.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      {'<leader>cp', ":CopilotChatToggle<CR>", { noremap = false }},
+      {'<leader>cm', ":CopilotChatModels<CR>", { noremap = false }},
+    }
+  },
+  {
     "zbirenbaum/copilot-cmp",
     dependencies = {
       {
