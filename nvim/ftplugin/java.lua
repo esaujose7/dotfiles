c13713ccec853a051121f9bfb7c02d1589b393a6
Esau Murillo <esaujose7@gmail.com>
2025-08-24 13:43:50 -0300

    moving stuff around once again

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -64,4 +64,4 @@ local config = {
 }
 
 require('jdtls').start_or_attach(config)
-require('wafle.util').on_attach()
+require('util').on_attach()
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,23 @@
-require("wafle")
+require("config.set")
+
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>h', ':wincmd h<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>j', ":wincmd j<CR>", { noremap = true })
+vim.keymap.set('n', '<leader>k', ":wincmd k<CR>", { noremap = true })
+vim.keymap.set('n', '<leader>l', ":wincmd l<CR>", { noremap = true })
+
+vim.keymap.set('n', "<C-d>", '<C-d>zz', { noremap = true })
+vim.keymap.set('n', "<C-u>", '<C-u>zz', { noremap = true })
+
+vim.keymap.set('n', 'n', "nzz", { noremap = true })
+vim.keymap.set('n', 'N', "Nzz", { noremap = true })
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+vim.lsp.config("*", {
+  on_attach = require 'config.util'.on_attach,
+  capabilities = capabilities,
+})
+
+require("config.lazy")
