1bfe8dcda3b31026fdf04776e707de0ee0225bb6
Esau Murillo <esaujose7@gmail.com>
2025-08-23 23:30:22 -0300

    add trouble

--- nvim/lua/wafle/plugins/editing.lua
+++ nvim/lua/wafle/plugins/editing.lua
@@ -43,6 +43,28 @@ return {
     }
   },
   {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
