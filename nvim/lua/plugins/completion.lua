f9b234bf4bb6ad8ee5999030ef3a6a282c393ec7
Esau Murillo <esaujose7@gmail.com>
2025-08-24 14:18:16 -0300

    remove unneeded mappings

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -21,11 +21,8 @@ return {
 
       cmp.setup({
         mapping = {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
-          ['<C-y>'] = cmp.config.disable,
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ['<Tab>'] = vim.schedule_wrap(function(fallback)
               if cmp.visible() and has_words_before() then
