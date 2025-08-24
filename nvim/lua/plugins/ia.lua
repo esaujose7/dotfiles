404c891886aa2a712e5ce2774c2f7bbabfd0715c
Esau Murillo <esaujose7@gmail.com>
2025-08-24 17:12:51 -0300

    use snacks picker for copilot chat autocomplete

--- nvim/lua/plugins/ia.lua
+++ nvim/lua/plugins/ia.lua
@@ -3,12 +3,29 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim", branch = "master" },
+      {
+        "folke/snacks.nvim",
+        opts = {
+          picker = {
+            enabled = true,
+            ui_select = true,
+          },
+        },
+      }
     },
     build = "make tiktoken",
     opts = {
       window = {
         width = 0.4,
       },
+      mappings = {
+        complete = {
+          insert = '<C-;>',
+          callback = function()
+            require('CopilotChat.completion').complete()
+          end,
+        },
+      }
     },
     keys = {
       {'<leader>a', ":CopilotChatToggle<CR>", { noremap = false }},
